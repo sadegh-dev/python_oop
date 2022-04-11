0698aff411b4a58278bae5c592166ca474af3652
sadegh-dev <sadegh.rez@gmail.com>
2022-04-11 12:30:01 +0430

    descriptors class

--- descriptors.py
+++ descriptors.py
@@ -5,3 +5,22 @@ class User:
     @property
     def name(self):
         return self.__name
+    
+    @name.setter
+    def name(self, name):
+        if name != 'admin':
+            self.__name = name
+        return self.__name
+
+    @name.deleter
+    def name(self):
+        self.__name = None
+
+p1 = User()
+
+p1.name = 'charlie'
+print(p1.name)
+
+del p1.name
+print(p1.name)
+
