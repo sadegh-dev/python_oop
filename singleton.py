d66f50ed9a7acda5d6a64d3944b49d7e47de5c9f
sadegh-dev <sadegh.rez@gmail.com>
2022-04-22 23:17:36 +0430

    finish Singleton

--- singleton.py
+++ singleton.py
@@ -1,8 +1,33 @@
 # Creational
 
 class Singleton(type):
-    __instance = None
+    _instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self._instance is None :
+            self._instance = super().__call__()
+        return self._instance
 
 
 class Db( metaclass = Singleton ):
     pass
+
+
+d1 = Db()
+d2 = Db()
+d3 = Db()
+
+
+print( id(d1) )
+print( id(d2) )
+print( id(d3) )
+
+
+"""
+[outPut]
+
+>>>> 140488319092672
+>>>> 140488319092672
+>>>> 140488319092672
+
+"""
