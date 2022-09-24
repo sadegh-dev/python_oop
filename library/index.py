ccc393f72665f67941f74a1fe71c810d81a24f49
sadegh-dev <sadegh.rez@gmail.com>
2022-09-24 11:26:54 +0330

    create library dir

---
+++ library/index.py
@@ -0,0 +1,15 @@
+
+class BookClass:
+    """
+    class Book is definition ,
+
+    This class includes details. 
+    """
+    def __init__(self, ISBN):
+        self.isbn = ISBN
+
+
+
+print(help(BookClass))
+
+
